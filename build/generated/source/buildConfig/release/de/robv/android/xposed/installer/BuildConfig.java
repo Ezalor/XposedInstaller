45ab0fe11453e4b0bc525807c00c4627f5586e7b
Ezalor <gggghhhh2924@163.com>
2017-03-20 10:46:55 +0000

    Build

---
+++ build/generated/source/buildConfig/release/de/robv/android/xposed/installer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.robv.android.xposed.installer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "de.robv.android.xposed.installer";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 37;
+  public static final String VERSION_NAME = "3.0.1 by dvdandroid - 26/11/16";
+}
